4a9765ac9d82494298353a291d0e128f0d29e795
amos-baron <amosbar1000@gmail.com>
2023-08-08 17:52:44 +0300

    added structure for sample rate convertion

--- helpers/audio_manipulators.py
+++ helpers/audio_manipulators.py
@@ -1,6 +1,19 @@
-def convert_to_required_format(audio_data, desiered_sample_rate, original_sample_rate):
-    if original_sample_rate != desiered_sample_rate:
-        # Implement sample rate convertion
-        return audio_data
-    else:
-        return audio_data
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+
+def convert_to_required_format(raw_data, desired_sample_rate, original_sample_rate):
+    # If original and desired sample rates are the same, return raw data
+    if original_sample_rate == desired_sample_rate:
+        logging.info("No sample rate conversion needed. Returning raw data.")
+        return raw_data
+
+    logging.info(f"Converting sample rate from {original_sample_rate} to {desired_sample_rate}")
+
+    # Sample rate convertion should happen here
+    resampled_audio = raw_data
+
+    logging.info("Sample rate conversion completed.")
+
+    return resampled_audio
