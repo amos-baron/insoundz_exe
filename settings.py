fee3de80a2e4b965d24f7ca3518e0f8f953a5b29
amos-baron <61280725+amos-baron@users.noreply.github.com>
2023-08-09 13:06:32 +0300

    Update settings.py
    added sample rate

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ class Settings(BaseSettings):
     app_interface: str = Field("127.0.0.1", env="APP_INTERFACE")
     app_port: int = Field(8080, env="APP_PORT")
     tf_model_path: str = Field('models/passthrough_model_lite.tflite',env='TF_MODEL_PATH')
+    desired_sample_rate: int = Field(48000,env='DESIRED_SAMPLE_RATE')
 
 settings = Settings()
